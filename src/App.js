21bf3a2afe6ee73f060e0c070242c661bdefa89f
Mary Etokwudo <etokwudomary@gmail.com>
2019-08-31 00:57:39 +0100

    design planets section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Banner from './components/Banner';
 import Starships from './components/Starships';
+import Planets from './components/Planets';
 
 function App() {
 	return (
 		<div>
 			<Banner></Banner>
 			<Starships></Starships>
+			<Planets></Planets>
 		</div>
 	);
 }
---
+++ src/components/Planets.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Image, Grid } from 'semantic-ui-react';
+
+export default function Planets() {
+	return (
+		<div style={{ padding: '0em 8em' }}  className='ui vertical center aligned segment'>
+			<div style={{margin: '3rem'}}>
+				<h2>Popular Planets</h2>
+				<hr style={{ width: '6rem', border: '0.2rem solid #393838' }}></hr>
+			</div>
+			<Grid stackable columns={3}>
+				<Grid.Row>
+					<Grid.Column
+						style={{
+							marginBottom: '20px',
+						}}
+					>
+						<Image
+							alt='planet'
+							style={{ height: '28rem', marginRight: '5rem', width: '24rem' }}
+							src='/assets/planet-1.jpg'
+						/>
+						<p
+							style={{
+								fontSize: '2em',
+								fontFamily: 'sans-serif',
+								fontWeight: '700px',
+								color: 'white',
+								position: 'absolute',
+								top: '90%',
+								left: '50%',
+								transform: 'translate(-50%, -50%)',
+							}}
+						>
+							Corelia
+						</p>
+					</Grid.Column>
+					<Grid.Column
+						style={{
+							marginBottom: '20px',
+						}}
+					>
+						<Image alt='planet' style={{ height: '28rem', marginRight: '5rem', width: '24rem' }} src='/assets/planet-2.jpg' />
+						<p
+							style={{
+								fontSize: '2em',
+								fontFamily: 'sans-serif',
+								fontWeight: '700px',
+								color: 'white',
+								position: 'absolute',
+								top: '90%',
+								left: '50%',
+								transform: 'translate(-50%, -50%)',
+							}}
+						>
+							Ord Mantell
+						</p>
+					</Grid.Column>
+					<Grid.Column
+						style={{
+							marginBottom: '20px',
+						}}
+					>
+						<Image alt='planet' style={{ height: '28rem', width: '24rem' }} src='/assets/planet-3.jpg' />
+						<p
+							style={{
+								fontSize: '2em',
+								fontFamily: 'sans-serif',
+								fontWeight: '700px',
+								color: 'white',
+								position: 'absolute',
+								top: '90%',
+								left: '50%',
+								transform: 'translate(-50%, -50%)',
+							}}
+						>
+							Endor
+						</p>
+					</Grid.Column>
+				</Grid.Row>
+			</Grid>
+		</div>
+	);
+}
