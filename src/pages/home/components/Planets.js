70e16d8c2321cd79a8f582117497b5a47eed12aa
Mary Etokwudo <etokwudomary@gmail.com>
2019-09-02 14:59:35 +0100

    refactor planets component without mapping

--- src/pages/home/components/Planets.js
+++ src/pages/home/components/Planets.js
@@ -7,11 +7,35 @@ import axios from 'axios';
 import CustomDotGroup from './CustomDotGroup';
 
 export default function Planets() {
-	const [planets, setplanets] = useState([]);
-	// const [slideIndex, setSlideIndex] = useState(0);
-let slideIndex = 0;
-
-	const images = ['/assets/planet-1.jpg', '/assets/planet-2.jpg', '/assets/planet-3.jpg'];
+	const [planets, setplanets] = useState([
+		{
+			name: 'Endor',
+		},
+		{
+			name: 'Endor',
+		},
+		{
+			name: 'Endor',
+		},
+		{
+			name: 'Endor',
+		},
+		{
+			name: 'Endor',
+		},
+		{
+			name: 'Endor',
+		},
+		{
+			name: 'Endor',
+		},
+		{
+			name: 'Endor',
+		},
+		{
+			name: 'Endor',
+		},
+	]);
 
 	useEffect(() => {
 		try {
@@ -23,82 +47,62 @@ let slideIndex = 0;
 		}
 	}, []);
 
-	function noOfSlides() {
-		if (planets.length && planets.length % 3) {
-			return Math.trunc(planets.length / 3) + 1;
-		} else if (planets.length && planets.length % 3 === 0) {
-			return planets.length / 3;
-		}
-		return 1;
-	}
-
-	// function slideIndex(index) {
-	// 	if (index > 2 && (Math.trunc(index / 3)> slideIndex)) {
-	// 		slideIndex += (Math.trunc(index / 3));
-	// 	}
-	// }
-
 	return (
 		<div className='ui center aligned sections'>
 			<div style={{ margin: '3rem', textAlign: 'center' }}>
 				<h2>Popular Planets</h2>
 				<hr className='sections-hr'></hr>
 			</div>
-			<CarouselProvider naturalSlideWidth={1} naturalSlideHeight={0.4} totalSlides={noOfSlides()}>
+			<CarouselProvider naturalSlideWidth={1} naturalSlideHeight={0.4} totalSlides={3}>
 				<Slider>
-					<Slide index={slideIndex}>
-						<Grid columns={3}>
-							{planets
-								? planets.map((planet, index) => {
-									if (index > 2 && (Math.trunc(index / 3)> slideIndex)) {
-										slideIndex += (Math.trunc(index / 3));
-									}
-										return (
-											<Grid.Column key={index}>
-												<Image
-													alt='planet'
-													className='planet-image'
-													src={index > 2 ? images[index % 3] : images[index]}
-												/>
-												<p className='planet-name'>{planet.name}</p>
-											</Grid.Column>
-										);
-								  })
-								: null}
-						</Grid>
-					</Slide>
-					{/* <Slide tag='a' index={0}>
+					<Slide index={0}>
 						<Grid columns={3}>
 							<Grid.Column>
 								<Image alt='planet' className='planet-image' src='/assets/planet-1.jpg' />
-								<p className='planet-name'>Corelia</p>
+								<p className='planet-name'>{planets[0].name}</p>
 							</Grid.Column>
 							<Grid.Column>
 								<Image alt='planet' className='planet-image' src='/assets/planet-2.jpg' />
-								<p className='planet-name'>Ord Mantell</p>
+								<p className='planet-name'>{planets[1].name}</p>
 							</Grid.Column>
 							<Grid.Column>
 								<Image alt='planet' className='planet-image' src='/assets/planet-3.jpg' />
-								<p className='planet-name'>Endor</p>
+								<p className='planet-name'>{planets[2].name}</p>
 							</Grid.Column>
 						</Grid>
 					</Slide>
-					<Slide tag='a' index={1}>
+					<Slide index={1}>
 						<Grid stackable columns={3}>
 							<Grid.Column>
 								<Image alt='planet' className='planet-image' src='/assets/planet-1.jpg' />
-								<p className='planet-name'>Corelia</p>
+								<p className='planet-name'>{planets[3].name}</p>
 							</Grid.Column>
 							<Grid.Column>
 								<Image alt='planet' className='planet-image' src='/assets/planet-2.jpg' />
-								<p className='planet-name'>Ord Mantell</p>
+								<p className='planet-name'>{planets[4].name}</p>
 							</Grid.Column>
 							<Grid.Column>
 								<Image alt='planet' className='planet-image' src='/assets/planet-3.jpg' />
-								<p className='planet-name'>Endor</p>
+								<p className='planet-name'>{planets[5].name}</p>
 							</Grid.Column>
 						</Grid>
-					</Slide> */}
+					</Slide>
+					<Slide index={3}>
+						<Grid stackable columns={3}>
+							<Grid.Column>
+								<Image alt='planet' className='planet-image' src='/assets/planet-1.jpg' />
+								<p className='planet-name'>{planets[6].name}</p>
+							</Grid.Column>
+							<Grid.Column>
+								<Image alt='planet' className='planet-image' src='/assets/planet-2.jpg' />
+								<p className='planet-name'>{planets[7].name}</p>
+							</Grid.Column>
+							<Grid.Column>
+								<Image alt='planet' className='planet-image' src='/assets/planet-3.jpg' />
+								<p className='planet-name'>{planets[8].name}</p>
+							</Grid.Column>
+						</Grid>
+					</Slide>
 				</Slider>
 				<CustomDotGroup slides={3} size='small' />
 			</CarouselProvider>
